9dc2ceedd3b1aee0051040139ad14165a0337928
alexarlord-boop <rezerw0.1@yandex.ru>
2024-03-07 13:57:32 +0200

    Deleted log

--- app/Http/Controllers/DatabaseTestController.php
+++ app/Http/Controllers/DatabaseTestController.php
@@ -9,7 +9,7 @@ class DatabaseTestController extends Controller
 {
     public function testDatabaseConnection()
     {
-        Log::info('Route hit!');
+//        Log::info('Route hit!');
         try {
             DB::connection()->getPdo();
             return "Database connection successful!";
