d9ce1d9c460a85d0ce47c346f6620c4ce7b66626
alexarlord-boop <rezerw0.1@yandex.ru>
2024-03-08 12:25:33 +0200

    Updated functionality classes

---
+++ app/Constants.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+
+class Constants
+{
+    const UPLOAD_DIR = 'uploads/';
+    const HOUR = 60;
+
+    const ENTITY_CACHE_LIVE = Constants::HOUR * 24;
+
+}
--- app/Http/Controllers/DatabaseTestController.php
+++ app/Http/Controllers/DatabaseTestController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+
 
 class DatabaseTestController extends Controller
 {
     public function testDatabaseConnection()
     {
-//        Log::info('Route hit!');
         try {
             DB::connection()->getPdo();
             return "Database connection successful!";
