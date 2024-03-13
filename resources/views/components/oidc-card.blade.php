f8ce629e7a1aa4cd189dca4d46d2f982867c1936
alexarlord-boop <rezerw0.1@yandex.ru>
2024-03-13 20:42:07 +0200

    Updated entity cards

--- resources/views/components/oidc-card.blade.php
+++ resources/views/components/oidc-card.blade.php
@@ -5,78 +5,65 @@
 
     <div class="card my-2" id="{{$id}}">
         <div class="card-header d-flex justify-content-between align-items-center">
-            <h5>{{$entity->getName()}}</h5>
+            <h4 class="m-0">{{$entity->getName()}}
+                <span class="p">
+                {{--            <span class="badge badge-secondary m-0">{{ $entity->getProtocol() }}</span>--}}
+                    {{--                <span class="badge badge-secondary m-0">{{ $entity->getSection() }}</span>--}}
+                <span class="badge badge-secondary m-0">{{ $entity->getType() }}</span></span>
+            </h4>
 
             <div class="btn-group" role="group" aria-label="Card Actions">
-{{--                <x-card-edit-btn :id="$id" :entity="$entity"/>--}}
+                {{--                <x-card-edit-btn :id="$id" :entity="$entity"/>--}}
                 <x-card-process-btn :id="$id" :entity="$entity" protocol="oidc"/>
             </div>
 
         </div>
-        <div class="card">
-            <div class="card-body">
+
+
+        <div class="">
+            <div class="card-body p-0">
                 <div class="row">
-                    <div class="col-md-6">
-                        <strong>Section:</strong> {{ $entity->getSection() }}<br>
-                        <strong>Protocol:</strong> {{ $entity->getProtocol() }}<br>
-                        <strong>Type:</strong> {{ $entity->getType() }}<br>
-                        <strong>Name:</strong> {{ $entity->getName() }}<br>
-                        <strong>Description:</strong> {{ $entity->getDescription() }}<br>
-                    </div>
-                    <div class="col-md-6">
-                        <strong>Resource Location:</strong> <a href="{{ $entity->getResourceLocation() }}">{{ $entity->getResourceLocation() }}</a><br>
-                        <strong>Entity ID:</strong> {{ $entity->getEntityId() }}<br>
-                        <strong>Dynamic Registration:</strong> {{ $entity->getDynamicRegistration() ? 'Yes' : 'No' }}<br>
-                        @if($entity->getClientSecret())
-                            <strong>Client Secret:</strong> {{ $entity->getClientSecret() }}<br>
-                        @endif
+                    <div class="col-md-12">
+                        <table class="table m-0">
+                            <tbody>
+                            <tr>
+                                {{--                                <th scope="row" style="width: 25%;">Name:</th>--}}
+                                {{--                                <td style="width: 25%;">{{ $entity->getName() }}</td>--}}
+                                {{--                                <th scope="row" style="width: 25%;">Section:</th>--}}
+                                {{--                                <td style="width: 25%;">{{ $entity->getSection() }}</td>--}}
+                            </tr>
+                            <tr>
+                                <th scope="row" style="width: 20%;">Description:</th>
+                                <td class="border-left">{{ $entity->getDescription() }}</td>
+                                {{--                                <th scope="row">Protocol:</th>--}}
+                                {{--                                <td>{{ $entity->getProtocol() }}</td>--}}
+                            </tr>
+                            <tr>
+                                <th scope="row">Redirect uri:</th>
+                                <td class="border-left">
+                                    <a href="{{ $entity->getResourceLocation() }}">{{ $entity->getResourceLocation() }}</a>
+                                </td>
+                                {{--                                <th scope="row">Type</th>--}}
+                                {{--                                <td>{{ $entity->getType() }}</td>--}}
+                            </tr>
+                            <tr>
+                                <th scope="row">Entity ID:</th>
+                                <td class="border-left">{{ $entity->getEntityId() }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Client Secret:</th>
+                                <td class="border-left" colspan="3">{{ $entity->getClientSecret() }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Dynamic Registration:</th>
+                                <td class="border-left">{{ $entity->getDynamicRegistration() ? 'Yes' : 'No' }}</td>
+                            </tr>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-
-
-{{--        <div class="card">--}}
-{{--            <div class="card-body p-0">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-12">--}}
-{{--                        <table class="table table-bordered m-0">--}}
-{{--                            <tbody>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row" style="width: 25%;">Name</th>--}}
-{{--                                <td style="width: 25%;">{{ $entity->getName() }}</td>--}}
-{{--                                <th scope="row" style="width: 25%;">Section</th>--}}
-{{--                                <td style="width: 25%;">{{ $entity->getSection() }}</td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row">Description</th>--}}
-{{--                                <td>{{ $entity->getDescription() }}</td>--}}
-{{--                                <th scope="row">Protocol</th>--}}
-{{--                                <td>{{ $entity->getProtocol() }}</td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row">Resource</th>--}}
-{{--                                <td><a href="{{ $entity->getResourceLocation() }}">{{ $entity->getResourceLocation() }}</a></td>--}}
-{{--                                <th scope="row">Type</th>--}}
-{{--                                <td>{{ $entity->getType() }}</td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row">Entity ID</th>--}}
-{{--                                <td>{{ $entity->getEntityId() }}</td>--}}
-{{--                                <th scope="row">Dynamic Registration</th>--}}
-{{--                                <td>{{ $entity->getDynamicRegistration() ? 'Yes' : 'No' }}</td>--}}
-{{--                            </tr>--}}
-{{--                            @if($entity->getClientSecret())--}}
-{{--                                <tr>--}}
-{{--                                    <th scope="row">Client Secret</th>--}}
-{{--                                    <td colspan="3">{{ $entity->getClientSecret() }}</td>--}}
-{{--                                </tr>--}}
-{{--                            @endif--}}
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
     </div>
 @endif
--- resources/views/components/saml-card.blade.php
+++ resources/views/components/saml-card.blade.php
@@ -2,45 +2,53 @@
 
     @php
         $id = uniqid();
+
+        $th_style = "";
+
     @endphp
-    <div class="card my-2" id="{{$id}}">
+    <div class="card my-3" id="{{$id}}">
         <div class="card-header d-flex justify-content-between align-items-center">
-            <h5 class="m-0">{{$entity->getName()}}</h5>
+            <h4 class="m-0">{{$entity->getName()}}
+            <span class="p">
+                {{--            <span class="badge badge-secondary m-0">{{ $entity->getProtocol() }}</span>--}}
+{{--                <span class="badge badge-secondary m-0">{{ $entity->getSection() }}</span>--}}
+                <span class="badge badge-secondary m-0">{{ $entity->getType() }}</span></span>
+            </h4>
+
+
 
             <div class="btn-group" role="group" aria-label="Card Actions">
 {{--                <x-card-edit-btn :id="$id" :entity="$entity" protocol="saml"/>--}}
                 <x-card-process-btn :id="$id" :entity="$entity" protocol="saml"/>
             </div>
-
         </div>
-
-        <div class="card">
+        <div class="">
                 <div class="card-body p-0">
                     <div class="row">
                         <div class="col-md-12">
-                            <table class="table table-bordered m-0">
+                            <table class="table m-0">
                                 <tbody>
                                 <tr>
-                                    <th scope="row" style="width: 25%;">Name*</th>
-                                    <td style="width: 25%;">{{ $entity->getName() }}</td>
-                                    <th scope="row" style="width: 25%;">Section</th>
-                                    <td style="width: 25%;">{{ $entity->getSection() }}</td>
+{{--                                    <th scope="row" style="width: 15%;">*Name:</th>--}}
+{{--                                    <td class="border-right" style="width: 35%;">{{ $entity->getName() }}</td>--}}
+{{--                                    <th scope="row" style="width: 25%;">Section:</th>--}}
+{{--                                    <td style="width: 25%;">{{ $entity->getSection() }}</td>--}}
                                 </tr>
                                 <tr>
-                                    <th scope="row">Description*</th>
-                                    <td>{{ $entity->getDescription() }}</td>
-                                    <th scope="row">Protocol</th>
-                                    <td>{{ $entity->getProtocol() }}</td>
+                                    <th scope="row" style="width: 15%;">Description:</th>
+                                    <td class="border-left">{{ $entity->getDescription() }}</td>
+{{--                                    <th scope="row">Protocol:</th>--}}
+{{--                                    <td>{{ $entity->getProtocol() }}</td>--}}
                                 </tr>
                                 <tr>
-                                    <th scope="row">Resource*</th>
-                                    <td><a href="{{ $entity->getResourceLocation() }}">{{ $entity->getResourceLocation() }}</a></td>
-                                    <th scope="row">Type*</th>
-                                    <td>{{ $entity->getType() }}</td>
+                                    <th scope="row">Metadata:</th>
+                                    <td class="border-left"><a href="{{ $entity->getResourceLocation() }}">{{ $entity->getResourceLocation() }}</a></td>
+{{--                                    <th scope="row">*Type:</th>--}}
+{{--                                    <td >{{ $entity->getType() }}</td>--}}
                                 </tr>
                                 <tr>
-                                    <th scope="row">Entity ID*</th>
-                                    <td>{{ $entity->getEntityId() }}</td>
+                                    <th scope="row">Entity ID:</th>
+                                    <td class="border-left">{{ $entity->getEntityId() }}</td>
                                 </tr>
                                 </tbody>
                             </table>
